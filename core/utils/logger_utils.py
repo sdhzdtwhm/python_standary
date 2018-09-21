f0deb6a1b408bd0821e478316a7cd229374bcf40
sdhzdtwhm <13716320887@139.com>
2018-09-21 13:48:53 +0800

    修改了logger_utils的方法

--- core/utils/logger_utils.py
+++ core/utils/logger_utils.py
@@ -28,6 +28,28 @@ class LoggerUtils:
         logger.addHandler(console_handle)
         return logger
 
+    def file_log(self, log_filename):
+        log_level = logging.DEBUG
+        formatter = logging.Formatter("%(asctime)s [filename:%(filename)s] [line:%(lineno)2d] [funcName:%(funcName)s]"
+                                      "[levelname:%(levelname)s] %(message)s")
+        handler = logging.handlers.RotatingFileHandler(log_filename, mode='a', encoding='utf-8', maxBytes=10*1024*1024, backupCount=5)
+        handler.setFormatter(formatter)
+        logger = logging.getLogger(log_filename)
+        logger.addHandler(handler)
+        logger.setLevel(log_level)
+        return logger
+
+    def console_log(self):
+        log_level = logging.DEBUG
+        formatter = logging.Formatter("%(asctime)s [filename:%(filename)s] [line:%(lineno)2d] [funcName:%(funcName)s]"
+                                      "[levelname:%(levelname)s] %(message)s")
+        logger = logging.getLogger()
+        logger.setLevel(log_level)
+        console_handle = logging.StreamHandler()
+        console_handle.setLevel(log_level)
+        console_handle.setFormatter(formatter)
+        logger.addHandler(console_handle)
+        return logger
 
 # def log():
 #     log_level = logging.DEBUG
